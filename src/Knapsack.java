da06ceb09b658e64ec40c14df2ddb76e86c0ba38
ywsoliman <ywsoliman@gmail.com>
2022-11-08 19:45:27 +0200

    Chromosome and Infeasible Solutions
    Created class "Chromosome" and fixed randomly generated population.

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Knapsack {
 
+    private final int numberOfItems;
     private int weight;
-    private int numberOfItems;
     private ArrayList<Item> items;
 
 
@@ -14,28 +15,64 @@ public class Knapsack {
         this.items = items;
     }
 
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int testCases = sc.nextInt();
+
+        int sizeOfKnapsack, numberOfItems, itemWeight, itemValue;
+        ArrayList<Item> items = new ArrayList<>();
+
+        for (int i = 0; i < testCases; i++) {
+
+            sizeOfKnapsack = sc.nextInt();
+            numberOfItems = sc.nextInt();
+            for (int j = 0; j < numberOfItems; j++) {
+                itemWeight = sc.nextInt();
+                itemValue = sc.nextInt();
+                items.add(new Item(itemWeight, itemValue));
+            }
+
+            Knapsack ks = new Knapsack(sizeOfKnapsack, numberOfItems, items);
+            ks.solve();
+
+        }
+
+    }
 
     public void solve() {
 
     }
 
-    private ArrayList<ArrayList<Integer>> InitializePopulation(int populationSize) {
+    private ArrayList<Chromosome> InitializePopulation(int populationSize) {
 
-        ArrayList<ArrayList<Integer>> chromosomes = new ArrayList<>(populationSize);
-        for(int item=0; item<numberOfItems; item++) {
+        ArrayList<Chromosome> totalChromosomes = new ArrayList<>(populationSize);
+
+        for (int item = 0; item < numberOfItems; item++) {
+
             ArrayList<Integer> chromosome = new ArrayList<>(numberOfItems);
-            for(int gene=0; gene<numberOfItems; gene++) {
 
-                chromosome.add((int) Math.round( Math.random()));
+            int fitnessValue = 0;
+
+            while (true) {
+
+                for (int gene = 0; gene < numberOfItems; gene++) {
+                    chromosome.add((int) Math.round(Math.random()));
+                    if (chromosome.get(gene) == 1) {
+                        fitnessValue += items.get(gene).value;
+                    }
+                }
+                if (fitnessValue <= weight) {
+                    totalChromosomes.add(new Chromosome(chromosome, fitnessValue));
+                    break;
+                }
+                fitnessValue = 0;
+                chromosome.clear();
             }
-            chromosomes.add(chromosome);
+
+
         }
-        return chromosomes;
-    }
-
-    private void FitnessEvaluation() {
-
+        return totalChromosomes;
     }
 
     private void Selection() {
@@ -54,14 +91,27 @@ public class Knapsack {
 
     }
 
-    static class Item {
-        int weight;
-        int value;
+    static class Chromosome {
+
+        private ArrayList<Integer> genes;
+        private int fitnessValue;
+
+        public Chromosome(ArrayList<Integer> genes, int fitnessValue) {
+            this.genes = genes;
+            this.fitnessValue = fitnessValue;
+        }
+
     }
 
-    public static void main(String[] args) {
+    static class Item {
 
+        private int weight;
+        private int value;
 
+        public Item(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
+        }
 
     }
 
